4f2a852a0da376aeaffa93127462b4d8164ef348
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-03 17:50:04 +0200

    prove myscript.js

---
+++ myscript.js
@@ -0,0 +1,12 @@
+console.log("L'elemento con l'ID jumbotron_section si è trasformato in una frase di testo");
+
+document.getElementById("jumbotron_section").innerHTML="Questo era il Jumbotron adesso è solo una frase";
+
+document.write("<a href=#jumbotron_section>Clicca qui per tornare a Jumbotron section</a>");
+
+document.write("<p>Paragrafo a fondo pagina</p>");
+
+function myFunction() {
+    alert("TOGGLE!!!");
+  }
+
