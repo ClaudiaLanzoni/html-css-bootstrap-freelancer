7f753471da8318c6214c7a4a6391becfe1a92208
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-04 09:20:34 +0200

    prova myscript.js su h1 contact_section

--- myscript.js
+++ myscript.js
@@ -1,12 +1,14 @@
 console.log("L'elemento con l'ID jumbotron_section si è trasformato in una frase di testo");
-
 document.getElementById("jumbotron_section").innerHTML="Questo era il Jumbotron adesso è solo una frase";
 
+console.log("Creazione link che rimandi a jumbotron_section:")
 document.write("<a href=#jumbotron_section>Clicca qui per tornare a Jumbotron section</a>");
 
 document.write("<p>Paragrafo a fondo pagina</p>");
 
-function myFunction() {
+function AlertSuToggle() {
     alert("TOGGLE!!!");
   }
 
+console.log("Questo elemento h1 della contact_section si è trasformato: " + document.getElementById("contact_h1").innerHTML);
+document.getElementById("contact_h1").innerHTML="CIAO H1"
